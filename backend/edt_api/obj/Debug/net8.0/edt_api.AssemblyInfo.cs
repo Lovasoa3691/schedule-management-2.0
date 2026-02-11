f6a23d8a1584f5285222908158d80c62b53888c6
Lovasoa3691 <fenonantenaikolovasoa@gmail.com>
2026-02-11 18:26:43 +0300

    commented backend build

--- backend/edt_api/obj/Debug/net8.0/edt_api.AssemblyInfo.cs
+++ backend/edt_api/obj/Debug/net8.0/edt_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("edt_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2213f7fb3bd54b75bd7e12a657c40b66060a6c22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c280064ada42070f9381b1181a6d84fbd30ea22e")]
 [assembly: System.Reflection.AssemblyProductAttribute("edt_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("edt_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
