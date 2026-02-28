3b64518150f23c5f91be23082cd2ec54b0a122d9
Lovasoa3691 <fenonantenaikolovasoa@gmail.com>
2026-02-28 21:37:49 +0300

    fdgdfhdf

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -199,7 +199,7 @@ const Dashboard = () => {
         <span className="font-bold">
           Sched<span className="text-blue-500">Connect</span>
         </span>
-        <span className="text-xs px-2 py-1 bg-green-100 text-green-700 rounded-full">
+        <span className="ml-6 text-xs px-2 py-2 bg-green-100 text-green-700 rounded-full">
           CI/CD OK
         </span>
       </h2>
