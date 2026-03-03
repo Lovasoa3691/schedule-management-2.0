060120663cbc13437869964ed6992c2719d042b0
Lovasoa3691 <fenonantenaikolovasoa@gmail.com>
2026-03-03 10:45:46 +0300

    change profile text

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -204,7 +204,7 @@ const Navbar = ({ setLoading }) => {
                       }`}
                     >
                       <FiUser className="w-5 h-5 mr-3 text-gray-800" />
-                      Profile
+                      Profil
                     </Link>
                   </li>
                 )}
